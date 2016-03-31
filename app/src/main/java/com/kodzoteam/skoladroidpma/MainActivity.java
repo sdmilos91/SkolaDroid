9d12c0266c91353ccce2c41146a48e40b1625624
Mladen <mladenkodzo@gmail.com>
2016-03-31 19:26:32 +0200

    Prvi komit Mladen

--- app/src/main/java/com/kodzoteam/skoladroidpma/MainActivity.java
+++ app/src/main/java/com/kodzoteam/skoladroidpma/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//prvi komit
         mSchools = new ArrayList<School>();
         mSchools.add(new School("Branislav Nusic", "Smederevo"));
         mSchools.add(new School("Ivo Andric", "Beograd"));
